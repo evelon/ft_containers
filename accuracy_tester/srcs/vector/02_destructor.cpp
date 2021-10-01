75b0f47b49426a0f635a28d7c56bea79e51b4b3f
jinbekim <59330110+jinbekim@users.noreply.github.com>
2021-10-01 13:14:27 +0900

    Fixed: #1 issue
    대괄호를 제거함.

--- accuracy_tester/srcs/vector/02_destructor.cpp
+++ accuracy_tester/srcs/vector/02_destructor.cpp
@@ -6,11 +6,9 @@ typedef std::list<std::string> strlist;
 int	main()
 {
 	strlist::iterator	it;
-	{
-		strlist	sv(4, "asfd");
-		NS::vector<strlist>	vec(4, sv);
-		it = vec.begin()->begin();
-	}
+	strlist	sv(4, "asfd");
+	NS::vector<strlist>	vec(4, sv);
+	it = vec.begin()->begin();
 	std::cout << *it << std::endl;
 	return (0);
 }
